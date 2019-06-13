fd4b79bd792b31476e4f1c3b49b6495df9278b6a
Piotr Kubisa <piotrkubisa@users.noreply.github.com>
2019-06-13 23:15:27 +0200

    Rename package import path

--- examples/themes.go
+++ examples/themes.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	prefixed "github.com/piotrkubisa/logrus-prefixed-formatter"
 	"github.com/sirupsen/logrus"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 var log = logrus.New()
